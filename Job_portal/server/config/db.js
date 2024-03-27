c21e001251551c5bc243ec1c2f9d1ec55f45b0af
praneeth vedagiri <96403254+praneeth-7606@users.noreply.github.com>
2024-03-27 18:46:28 +0000

    Add new feature

--- Job_portal/server/config/db.js
+++ Job_portal/server/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 // import colors from "colors"
 const database=async()=>{
     try{
-        const conn =await mongoose.connect("mongodb://localhost:27017/mylogin");
+        const cons =await mongoose.connect(`mongodb+srv://mynewuser:number1234@cluster0.dzro3qd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
         console.log(`connected to mongodb database`)
     }catch(error){
         console.log(`error in mongodb ${error}`)
@@ -12,3 +12,20 @@ const database=async()=>{
 }
 
 export default database
+// import mongoose from "mongoose";
+
+// const database = async () => {
+//   try {
+//     const uri = process.env.MONGODB_URL || "mongodb://localhost:27017/mylogin";
+//     const conn = await mongoose.connect(uri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     });
+//     console.log(`Connected to MongoDB at ${uri}`);
+//   } catch (error) {
+//     console.error(`Error connecting to MongoDB: ${error.message}`);
+//     process.exit(1); // Exit with failure
+//   }
+// };
+
+// export default database;
